ce7c37246f5b4dbbd27fb01269583afa57c368e6
marievy <ivymarieporras@gmail.com>
2025-03-25 16:17:12 -0600

    add: bgm and click sound

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const clickSound = new Audio('click-pixabay.mp3');
+const bgm = new Audio('game-music-pixabay.mp3');
+
 // Get a random joke
 function getJoke(category) {
    fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
@@ -16,12 +19,33 @@ function getJoke(category) {
 
 // Get a new joke when the button is clicked
 ["programming", "miscellaneous", "pun", "spooky", "christmas"].forEach(id =>
-   document.getElementById(id).addEventListener("click", () =>
-      getJoke(id)
+   document.getElementById(id).addEventListener("click", () => {
+      clickSound.currentTime = 0;
+      clickSound.play();
+      getJoke(id);
+   }
    )
 );
 
 getJoke("Any");
+
+// Try to play automatically on page load
+window.addEventListener('load', () => {
+   bgm.play().catch(err => {
+      console.warn('Autoplay was prevented:', err);
+   });
+});
+
+bgmToggle.addEventListener('click', () => {
+   if (bgm.paused) {
+      bgm.play();
+      bgmToggle.textContent = 'Pause Music';
+   } else {
+      bgm.pause();
+      bgmToggle.textContent = 'Play Music';
+   }
+});
+bgm.loop = true;
 
 // changing joke container
 const update = () => jokeImgContainer.src = window.innerWidth <= 600 ? 'images/jokecontainer-mobile.webp' : 'images/jokecontainer.webp';
